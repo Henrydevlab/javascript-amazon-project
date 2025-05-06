883a37c8e637e520374406abf64b1cf494ea32b7
Henry <henrydevlab@gmail.com>
2025-05-06 17:37:03 +0300

    Save data and generate HTML for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
-import { formartCurrency } from '../utils/money.js';
+import {products, getProduct} from '../../data/products.js';
+import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 
 hello();
 
@@ -17,23 +17,11 @@ export function renderOrderSummary() {
     cart.forEach((cartItem) => {
         const productId = cartItem.productId;
 
-        let matchingProduct;
-
-        products.forEach((product) => {
-            if (product.id === productId) {
-                matchingProduct = product;
-            }
-        });
+        const matchingProduct = getProduct(productId);
 
         const deliveryOptionId = cartItem.deliveryOptionId;
 
-        let deliveryOption;
-
-        deliveryOptions.forEach((option) => {
-        if (option.id === deliveryOptionId) {
-            deliveryOption = option;
-        }
-        });
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
 
         const today = dayjs();
         const deliveryDate = today.add(
@@ -60,7 +48,7 @@ export function renderOrderSummary() {
                         ${matchingProduct.name}
                     </div>
                     <div class="product-price">
-                        $${formartCurrency(matchingProduct.priceCents)}
+                        $${formatCurrency(matchingProduct.priceCents)}
                     </div>
                     <div class="product-quantity">
                         <span>
@@ -101,7 +89,7 @@ export function renderOrderSummary() {
 
             const priceString = deliveryOption.priceCents === 0
                 ? 'FREE'
-                : `$${formartCurrency(deliveryOption.priceCents)} -`;
+                : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
             const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
